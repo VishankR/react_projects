8caee1f54bf88b837280c22e5679ce22aafb76ff
VishankR <vishankrajput2001@gmail.com>
2023-11-10 19:43:44 +0530

    Added gitignore file on Nov 10, 2023

---
+++ city-tours/src/TourData.js
@@ -0,0 +1,32 @@
+export const tourData = [
+    {
+        id: 1,
+        city: "New York",
+        img: "./img/newyork.webp",
+        name: "New York Bridge Tour",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, officiis."
+    },
+    {
+        id: 2,
+        city: "Paris",
+        img: "./img/paris.jpg",
+        name: "Paris Lights Tour",
+        info: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam, et."
+    },
+    {
+        id: 3,
+        city: "London",
+        img: "./img/london.jpg",
+        name: "London Royal Palace Tour",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, optio!"
+    },
+    {
+        id: 4,
+        city: "Tokyo",
+        img: "./img/tokyo.jpg",
+        name: "Tokyo Sushi Tower",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium, deleniti?"
+    },
+]
+
+export default tourData;
